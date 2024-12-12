36e51eee13d3e8f5ec8693c23560acf578e97add
hendisantika <hendisantika@yahoo.co.id>
2024-12-12 16:36:46 +0700

    Add upload in AWSS3Controller🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/webfluxawss3v2/controller/AWSS3Controller.java
+++ src/main/java/id/my/hendisantika/webfluxawss3v2/controller/AWSS3Controller.java
@@ -1,10 +1,16 @@
 package id.my.hendisantika.webfluxawss3v2.controller;
 
+import id.my.hendisantika.webfluxawss3v2.common.FileUtils;
+import id.my.hendisantika.webfluxawss3v2.domain.SuccessResponse;
 import id.my.hendisantika.webfluxawss3v2.service.AWSS3FileStorageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +29,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AWSS3Controller {
 
     private final AWSS3FileStorageService fileStorageService;
+
+    @PostMapping("/upload")
+    public Mono<SuccessResponse> upload(@RequestPart("file-data") Mono<FilePart> filePart) {
+        return filePart
+                .map(file -> {
+                    FileUtils.filePartValidator(file);
+                    return file;
+                })
+                .flatMap(fileStorageService::uploadObject)
+                .map(fileResponse -> new SuccessResponse(fileResponse, "Upload successfully"));
+    }
 }
