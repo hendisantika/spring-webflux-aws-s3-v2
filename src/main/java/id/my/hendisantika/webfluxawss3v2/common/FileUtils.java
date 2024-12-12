9645890954de31f3209335dd4f07f130392fd9ce
hendisantika <hendisantika@yahoo.co.id>
2024-12-12 16:22:33 +0700

    Add FileUtils🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/webfluxawss3v2/common/FileUtils.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.webfluxawss3v2.common;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-webflux-aws-s3-v2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/12/24
+ * Time: 16.21
+ * To change this template use File | Settings | File Templates.
+ */
+@UtilityClass
+@Slf4j
+public class FileUtils {
+    private final String[] contentTypes = {
+            "image/png",
+            "image/jpg",
+            "image/jpeg",
+            "image/bmp",
+            "image/gif",
+            "image/ief",
+            "image/pipeg",
+            "image/svg+xml",
+            "image/tiff"
+    };
+}
